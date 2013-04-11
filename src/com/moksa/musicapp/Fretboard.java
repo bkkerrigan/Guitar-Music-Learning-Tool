842959daf5d56dfa70e7b0ee5abb3c391e8e2f20
bkkerrigan <kerrigan.brendan@gmail.com>
2013-04-11 13:32:36 -0300

    Update Fretboard.java

--- src/com/moksa/musicapp/Fretboard.java
+++ src/com/moksa/musicapp/Fretboard.java
@@ -17,7 +17,10 @@ public class Fretboard implements ActionListener{
     ImageIcon white = new ImageIcon(getClass().getResource("white.jpg"));
     ImageIcon black = new ImageIcon(getClass().getResource("black.jpg"));
     ImageIcon red = new ImageIcon(getClass().getResource("red.jpg"));
-
+    
+    
+    
+    ArrayList<ArrayList<JButton>> guitarStrings;
     ArrayList stringOne;
     ArrayList stringTwo;
     ArrayList stringThree;
@@ -65,7 +68,7 @@ public class Fretboard implements ActionListener{
             }
 
             note.setName("Button" + Integer.toString(f));
-            note.addActionListener(this);
+            note.addActionListener(new chordListener);
             buttonsArray.add(note);
         }
 
@@ -75,8 +78,34 @@ public class Fretboard implements ActionListener{
     private class chordListener implements ActionListener{
 
         public void actionPerformed(ActionEvent cA) {
-
-            for(int f = 0; f < 3; f++);
+            
+            
+            ArrayList<JButton> currentString;
+            JButton currentNote;
+            
+            for(int f = 0; f < 6; f++){
+                
+                currentString = guitarStrings.get(f);
+                
+                for(int p = 0; p < 3; p++){
+                    
+                    currentNote = currentString.get(p);
+                    
+                    for(int n = 0; n < lengthOfNotes; n++){
+                        
+                        if(currentNote.getActionCommand == scaleNotes[n]){
+                            
+                            currentNote.setIcon("red.jpg");
+                            
+                        }
+                        
+                    }
+                     
+                    
+                }
+                
+                
+            }
 
 
         }
